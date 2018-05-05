2961df96272c5311b5c5da8da1435992db489771
Melike <melikeb.ertekin@gmail.com>
2018-05-04 22:55:18 -0400

    Add Timer

--- js/app.js
+++ js/app.js
@@ -1,4 +1,54 @@
 //Functions
+var second = 0, minute = 0, hour = 0;
+var timer = document.querySelector(".timer");
+var interval;
+
+// Start Timer
+function startTimer(){
+    interval = setInterval(function(){
+    	if(hour<10 && minute<10 && second<10){
+    		timer.innerHTML = "0"+hour+ " : "+"0"+minute+" : 0"+second;
+    	}
+    	else if(hour<10 && minute<10 && second>10){
+       		timer.innerHTML = "0"+hour+" : 0"+minute+" : "+second;
+    	}
+    	else if(hour<10 && minute>10 && second<10){
+       		timer.innerHTML = "0"+hour+" : "+minute+" : 0"+second;
+    	}
+    	else if(hour<10 && minute>10 && second>10){
+       		timer.innerHTML = "0"+hour+" : "+minute+" : "+second;
+    	}
+    	else if(hour>10 && minute<10 && second<10){
+       		timer.innerHTML = hour+" : 0"+minute+" : 0"+second;
+    	}
+    	else if(hour>10 && minute<10 && second>10){
+       		timer.innerHTML = hour+" : 0"+minute+" : "+second;
+    	}
+    	else if(hour>10 && minute>10 && second<10){
+       		timer.innerHTML = hour+" : "+minute+" : 0"+second;
+    	}
+		else if(second>10 && minute>10 && hour>10){
+    		timer.innerHTML = hour+ " : "+minute+" : "+second;
+    	}
+
+        second++;
+        if(second == 60){
+            minute++;
+            second = 0;
+        }
+        if(minute == 60){
+            hour++;
+            minute = 0;
+        }
+    },1000);
+}
+
+//Stop Timer
+function stopTimer(){
+	second = 0, minute = 0, hour = 0;
+	timer.innerHTML = "00 : 00 : 00";
+	clearInterval(interval);
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -54,7 +104,7 @@ function checkMatch() {
 		var card1 = openCards[0].children[0].classList.value;
 		var card2 = openCards[1].children[0].classList.value;
 
-		if (card1 === card2){
+		if(card1 === card2){
 			openCards[0].classList.add('match');
 			openCards[1].classList.add('match');
 
@@ -62,6 +112,13 @@ function checkMatch() {
 			openCards[0].classList.remove('show');
 			openCards[1].classList.remove('open');
 			openCards[1].classList.remove('show');
+
+			var openCards = $('.match');
+			if(openCards.length===16){
+				$('#theEndModel').modal('show');
+				stopTimer();
+				//Console.log(timer);
+			}
 		}
 	}
 }
@@ -69,6 +126,7 @@ function checkMatch() {
 //game starts
 $( document ).ready(function() {
 	restart();
+	startTimer();
 });
 
 
@@ -76,19 +134,32 @@ $( document ).ready(function() {
 var moves=0;
 var counter=0;
 $('.card').click(function() {
+
 	if(!$(this).hasClass('match')){
 		if (counter<2) {
 			$(this).toggleClass('open show');
 			counter++;
 			moves++;
-			$('.moves').text(moves);
+
+			if(moves<2){
+				$('.moves').text(moves+" Move");
+			}
+			else{
+				$('.moves').text(moves+" Moves");
+			}
 		}
 		else {
 			counter=1;
 			$('.card').removeClass('open show');
 			$(this).toggleClass('open show');
 			moves++;
-			$('.moves').text(moves);
+
+			if(moves<2){
+				$('.moves').text(moves+" Move");
+			}
+			else{
+				$('.moves').text(moves+" Moves");
+			}
 		}
 
 
@@ -99,9 +170,33 @@ $('.card').click(function() {
 });
 
 
+$('.play-again').click(function() {
+	moves=0;
+
+	if(moves<2){
+		$('.moves').text(moves+" Move");
+	}
+	else{
+		$('.moves').text(moves+" Moves");
+	}
+	stopTimer();
+	startTimer();
+	restart();
+
+
+});
+
+
 $('.restart').click(function() {
 	moves=0;
-	$('.moves').text(moves);
+	if(moves<2){
+		$('.moves').text(moves+" Move");
+	}
+	else{
+		$('.moves').text(moves+" Moves");
+	}
+	stopTimer();
+	startTimer();
 	restart();
 
 });
